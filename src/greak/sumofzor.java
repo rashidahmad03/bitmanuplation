094f287da9ec3d42fa982a59d44bebecaa3d9271
rashidahmad03 <rashidahmad2135@gmail.com>
2019-11-21 18:19:51 +0530

    no message

--- src/greak/sumofzor.java
+++ src/greak/sumofzor.java
@@ -1,10 +1,25 @@
 package greak;
 
+import java.util.Scanner;
+
 public class sumofzor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			System.out.println(countlop(n));
 	}
-fsdf
+	public static int countlop(int n) {
+		int c=0;
+		for(int i=0;i<n;i++) {
+			if((i+n)==(n^i))
+				c++;
+		}
+		return c;
+	}
+	public static int bitcount(int n) {
+		return n;
+	}
+
 }
